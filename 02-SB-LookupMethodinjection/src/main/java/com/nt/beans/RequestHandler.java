4c9c753746ecef380f1054e8ae4e0b9d7b2c0549
mukesh <mukesh@DESKTOP-79CHHOH>
2020-08-18 01:30:11 +0530

    lmi

---
+++ 02-SB-LookupMethodinjection/src/main/java/com/nt/beans/RequestHandler.java
@@ -0,0 +1,24 @@
+package com.nt.beans;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mukesh
+ *
+ */
+@Component("rh")
+@Scope("prototype")
+public class RequestHandler {
+	private static int count;
+	
+	public RequestHandler() {
+		count++;
+		System.out.println("RequestHandler::0-param  constructor::" +count);
+	}
+	
+	public   void  handle(String data) {
+		System.out.println("handling request using object ::"+count + "having data :::"+data +"-->"+this.hashCode());
+	}
+
+}
